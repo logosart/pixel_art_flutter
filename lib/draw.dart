270b3600015b29db3ecb0b6d187429647f286f80
LUCAS SOUZA ARAUJO <07553083100@tocantins.governo>
2024-04-29 10:23:22 -0300

    transformando draw em canvas

--- lib/draw.dart
+++ lib/draw.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'dart:typed_data';
 import 'dart:ui' as ui;
+import 'dart:typed_data';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 class DrawPage extends StatefulWidget {
@@ -18,8 +17,6 @@ class _DrawPageState extends State<DrawPage> {
   List<List<Color>> pixels = [];
   Color _selectedColor = Colors.black;
   Color _originalColor = Colors.white;
-
-  final GlobalKey _globalKey = GlobalKey(); // Adicionando GlobalKey
 
   @override
   void initState() {
@@ -54,47 +51,30 @@ class _DrawPageState extends State<DrawPage> {
           _buildColorPalette(),
           _buildToolPalette(),
           Expanded(
-            child: Center(
-              child: RepaintBoundary( // Adicionando RepaintBoundary com GlobalKey
-                key: _globalKey,
-                child: SizedBox(
-                  width: tamanhoPixel * widget.largura,
-                  height: tamanhoPixel * widget.altura,
-                  child: GridView.builder(
-                    itemCount: widget.altura * widget.largura,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: widget.largura,
-                    ),
-                    itemBuilder: (context, index) {
-                      int linha = index ~/ widget.largura;
-                      int coluna = index % widget.largura;
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            pixels[linha][coluna] =
-                                _selectedColor == _originalColor
-                                    ? Colors.white
-                                    : _selectedColor;
-                          });
-                        },
-                        child: Container(
-                          width: tamanhoPixel,
-                          height: tamanhoPixel,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            color: pixels[linha][coluna],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
+            child: GestureDetector(
+              onPanUpdate: (details) {
+                setState(() {
+                  RenderBox renderBox = context.findRenderObject() as RenderBox;
+                  Offset localPosition = renderBox.globalToLocal(details.globalPosition);
+                  int coluna = (localPosition.dx / tamanhoPixel).floor();
+                  int linha = (localPosition.dy / tamanhoPixel).floor();
+                  if (linha >= 0 &&
+                      linha < widget.altura &&
+                      coluna >= 0 &&
+                      coluna < widget.largura) {
+                    pixels[linha][coluna] = _selectedColor == _originalColor ? Colors.white : _selectedColor;
+                  }
+                });
+              },
+              child: CustomPaint(
+                size: Size(tamanhoPixel * widget.largura, tamanhoPixel * widget.altura),
+                painter: DrawingPainter(pixels: pixels, tamanhoPixel: tamanhoPixel),
               ),
             ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton( // Adicionando FloatingActionButton para salvar
+      floatingActionButton: FloatingActionButton(
         onPressed: _captureImage,
         child: Icon(Icons.save),
       ),
@@ -158,7 +138,6 @@ class _DrawPageState extends State<DrawPage> {
   Widget _buildToolButton(String toolName) {
     return GestureDetector(
       onTap: () {
-        // Adicione ação para a ferramenta selecionada
         if (toolName == 'Borracha') {
           setState(() {
             _selectedColor = _originalColor;
@@ -180,14 +159,38 @@ class _DrawPageState extends State<DrawPage> {
 
   void _captureImage() async {
     try {
-      RenderRepaintBoundary boundary = _globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
-      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
-      ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-      Uint8List pngBytes = byteData!.buffer.asUint8List();
-      final result = await ImageGallerySaver.saveImage(pngBytes); // Salva a imagem na galeria
+      final recorder = ui.PictureRecorder();
+      final canvas = Canvas(recorder, Rect.fromPoints(Offset(0.0, 0.0), Offset(widget.largura.toDouble(), widget.altura.toDouble())));
+      DrawingPainter(pixels: pixels, tamanhoPixel: 20.0).paint(canvas, Size(widget.largura.toDouble() * 20.0, widget.altura.toDouble() * 20.0));
+      final picture = recorder.endRecording();
+      final img = await picture.toImage(widget.largura * 20, widget.altura * 20);
+      final pngBytes = await img.toByteData(format: ui.ImageByteFormat.png);
+      final result = await ImageGallerySaver.saveImage(Uint8List.view(pngBytes!.buffer)); // Save image to gallery
       print(result);
     } catch (e) {
       print(e);
     }
+  }
+}
+
+class DrawingPainter extends CustomPainter {
+  final List<List<Color>> pixels;
+  final double tamanhoPixel;
+
+  DrawingPainter({required this.pixels, required this.tamanhoPixel});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int linha = 0; linha < pixels.length; linha++) {
+      for (int coluna = 0; coluna < pixels[linha].length; coluna++) {
+        Paint paint = Paint()..color = pixels[linha][coluna];
+        canvas.drawRect(Rect.fromLTWH(coluna * tamanhoPixel, linha * tamanhoPixel, tamanhoPixel, tamanhoPixel), paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
